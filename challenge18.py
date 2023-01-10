e5c1bbaa1504302de8285b17607307016c629fb6
brandonyrigan <brandonayrigan@gmail.com>
2023-01-10 16:31:15 +0000

    join and split

---
+++ challenge18.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    name = input("What's your name?: ")
+    day = input("What day of the week is it?: ")
+
+    print("Hello " + name + "! Happy " + day + "!")
+
+main()
