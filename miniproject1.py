7a57954334588e33a567d5b9ea2a1ee72a594284
brandonyrigan <brandonayrigan@gmail.com>
2023-01-12 20:31:15 +0000

    studying for logic

--- miniproject1.py
+++ miniproject1.py
@@ -12,7 +12,7 @@ def main():
     print()
     
     ## Create a choice array for user to choose from
-    choices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    choices = [1, 2, 3, 4, 5, 6, 7, 8]
 
     ## Generate a random number for monster to take damage
     ## Initialize monster hp and number of tries
@@ -26,14 +26,17 @@ def main():
     ## else if monster hp is 0, user wins game
     while monster_hp > 0:
         try:
-            user_guess = int(input("Choose a number between 1 and 10 to see if you hit the monster!: "))
+            user_guess = int(input("Choose a number between 1 and 8 to see if you hit the monster!: "))
         
-            if user_guess > 10 or user_guess < 1:
-                print("Please choose a number between 1 and 10")
+            if user_guess > 8 or user_guess < 1:
+                print("Please choose a number between 1 and 8")
                 continue
 
             if user_guess == monster_number:
                 monster_hp = monster_hp - 5
+                if monster_hp == 0:
+                    print("Congrats! You defeated the monster!")
+                    break
                 print(f"Nice! You hit the monster. It has {monster_hp}hp left.")
                 monster_number = random.choice(choices)
                 tries = tries + 1
@@ -44,13 +47,10 @@ def main():
             else:
                 tries = tries - 1
                 print(f"Sorry, you didn't hit. You have {tries} tries left.")
-        
-            if monster_hp == 0:
-                print("Congrats! You defeated the monster!")
-                break
         except ValueError as err:
             print("That was not a legal value for division:", err)
 
     print("Exiting program.")
 
-main()
+if __name__ == "__main__":
+    main()
