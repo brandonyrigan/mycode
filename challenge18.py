622ebd3626a6b40220787f4baa104f8ff5d5ac86
brandonyrigan <brandonayrigan@gmail.com>
2023-01-20 22:31:36 +0000

    clean up

--- challenge18.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    name = input("What's your name?: ")
-    day = input("What day of the week is it?: ")
-
-    print("Hello " + name + "! Happy " + day + "!")
-
-main()
